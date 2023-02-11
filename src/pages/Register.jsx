28c943eb870ebb6b9501046531cfb2418e8bae45
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 09:45:16 -0600

    changed input placeholder text

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
         <h1>ChatUp</h1>
         <h3>Register</h3>
         <form className="register--form">
-          <input type="text" name="name" placeholder="name"></input>
+          <input type="text" name="name" placeholder="username"></input>
           <input type="email" name="email" placeholder="email"></input>
           <input type="password" name="password" placeholder="password"></input>
           <label id="avatar-picker">
