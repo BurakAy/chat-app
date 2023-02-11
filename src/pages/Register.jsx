dd92a11059db1211a3b7ed26d71f5fc36b1227fd
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 09:42:10 -0600

    created register component

---
+++ src/pages/Register.jsx
@@ -0,0 +1,31 @@
+import "../styles/Register.css";
+import imgIcon from "../assets/avatar_icon.svg";
+
+const Register = () => {
+  return (
+    <div className="register--container">
+      <div className="register--wrapper">
+        <h1>ChatUp</h1>
+        <h3>Register</h3>
+        <form className="register--form">
+          <input type="text" name="name" placeholder="name"></input>
+          <input type="email" name="email" placeholder="email"></input>
+          <input type="password" name="password" placeholder="password"></input>
+          <label id="avatar-picker">
+            <img src={imgIcon} /> Choose an avatar
+            <input type="file" name="avatar" accept=".png, .jpg, .jpeg"></input>
+          </label>
+          <button type="submit">Sign In</button>
+        </form>
+        <p className="register--sign_up">
+          Already have an account?&nbsp;
+          <a href="" target="_blank">
+            Login
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
