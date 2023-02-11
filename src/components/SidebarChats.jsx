d6431d4fa76dcf1c7578e4c89c933dbf57fd256f
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 14:46:00 -0600

    sidebar chat elements and  styling

--- src/components/SidebarChats.jsx
+++ src/components/SidebarChats.jsx
@@ -4,9 +4,19 @@ import userImage from "../assets/Burak_Ephesus.jpg";
 const SidebarChats = () => {
   return (
     <div className="sidebarchats--container">
-      <div className="sidebarchats">
+      <div className="sidebarchat">
         <img src={userImage} />
-        <p>Burak Aydemir</p>
+        <div className="sidebarchat--chat_info">
+          <p className="sidebarchat--user">Burak Aydemir</p>
+          <p className="sidebarchat--user_msg">Hey, what are you up to?</p>
+        </div>
+      </div>
+      <div className="sidebarchat">
+        <img src={userImage} />
+        <div className="sidebarchat--chat_info">
+          <p className="sidebarchat--user">Burak Aydemir</p>
+          <p className="sidebarchat--user_msg">Hey, what are you up to?</p>
+        </div>
       </div>
     </div>
   );
