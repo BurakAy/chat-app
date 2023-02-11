74105e0abfa0c84cfbec17e47058dd5a4824bf8a
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 13:05:20 -0600

    sidebar navbar styling

--- src/components/SidebarNavbar.jsx
+++ src/components/SidebarNavbar.jsx
@@ -1,7 +1,16 @@
+import "../styles/SidebarNavbar.css";
+
 const SidebarNavbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="side-navbar--container">
+      <div className="side-navbar--wrapper">
+        <p className="side-navbar__appname">ChatUp</p>
+        <div className="side-navbar__user">
+          <img src="" className="side-navbar__user-avatar" />
+          <p>Burak Aydemir</p>
+        </div>
+        <button className="side-navbar__logout">logout</button>
+      </div>
     </div>
   );
 };
