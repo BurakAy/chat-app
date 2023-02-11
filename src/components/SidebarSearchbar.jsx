253a87f463782daee2d4a4b58fa2d840e79f356b
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 13:32:50 -0600

    changed form elmnt to div

--- src/components/SidebarSearchbar.jsx
+++ src/components/SidebarSearchbar.jsx
@@ -4,14 +4,14 @@ const SidebarSearchbar = () => {
   return (
     <div className="searchbar--container">
       <div className="searchbar--wrapper">
-        <form className="searchbar--form">
+        <div className="searchbar--form">
           <input
             className="finduser"
             name="finduser"
             type="text"
             placeholder="Find a user"
           />
-        </form>
+        </div>
       </div>
     </div>
   );
