aa86a0076e2123013cd0d0d05cb1e55f607b7257
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-12 07:54:35 -0600

    handling userInfo input values

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,63 @@
 import "../styles/Register.css";
 import imgIcon from "../assets/avatar_icon.svg";
 import PhotoIcon from "@mui/icons-material/Photo";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Register = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+    avatar: "",
+  });
+
+  const handleUserInfoChange = (event) => {
+    const name = event.target.name;
+    setUserInfo((prevVal) => {
+      return {
+        ...prevVal,
+        [name]: event.target.value,
+      };
+    });
+  };
+
   return (
     <div className="register--container">
       <div className="register--wrapper">
         <h1>ChatUp</h1>
         <h3>Register</h3>
         <form className="register--form">
-          <input type="text" name="name" placeholder="username"></input>
-          <input type="email" name="email" placeholder="email"></input>
-          <input type="password" name="password" placeholder="password"></input>
+          <input
+            type="text"
+            name="name"
+            placeholder="username"
+            value={userInfo.name}
+            onChange={handleUserInfoChange}
+          ></input>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={userInfo.email}
+            onChange={handleUserInfoChange}
+          ></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={userInfo.password}
+            onChange={handleUserInfoChange}
+          ></input>
           <label id="avatar-picker">
             <PhotoIcon fontSize="large" /> Add an avatar
-            <input type="file" name="avatar" accept=".png, .jpg, .jpeg"></input>
+            <input
+              type="file"
+              name="avatar"
+              accept=".png, .jpg, .jpeg"
+              value={userInfo.avatar}
+              onChange={handleUserInfoChange}
+            ></input>
           </label>
           <button type="submit">Sign Up</button>
         </form>
