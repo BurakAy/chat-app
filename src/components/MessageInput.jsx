a0578bfee08de2c03fa96f14dc32ca060c03c99f
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 20:07:49 -0600

    messageinput file upload elements, submit button, and styling

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,10 +1,23 @@
 import "../styles/MessageInput.css";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import PhotoIcon from "@mui/icons-material/Photo";
 
 const MessageInput = () => {
   return (
     <div className="messageinput--container">
       <div className="messageinput--wrapper">
         <input type="text" placeholder="Enter a message..." />
+        <div className="messageinput--icons">
+          <label className="messageinput__file">
+            <AttachFileIcon fontSize="medium" />
+            <input name="file" type="file" />
+          </label>
+          <label className="messageinput__img">
+            <PhotoIcon fontSize="medium" />
+            <input name="img" type="file" />
+          </label>
+          <button type="submit">Send</button>
+        </div>
       </div>
     </div>
   );
