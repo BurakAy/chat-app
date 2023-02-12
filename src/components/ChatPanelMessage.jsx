8741a4859a83cc1b6fa54129bdd5b36f250ac13c
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 19:21:19 -0600

    sent message styling and html

--- src/components/ChatPanelMessage.jsx
+++ src/components/ChatPanelMessage.jsx
@@ -5,8 +5,11 @@ const ChatPanelMessage = () => {
   return (
     <div className="chatpanelmessage--container">
       <div className="chatpanelmessage--wrapper">
-        <img src={userImage} />
-        <p>message</p>
+        <div className="chatpanelmessage--msg_info">
+          <img src={userImage} />
+          <span>time</span>
+        </div>
+        <p className="chatpanelmessage--sent_msg">message</p>
       </div>
     </div>
   );
