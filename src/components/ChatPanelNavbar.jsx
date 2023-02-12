15648f99784d612a1c7dcc98513a1250db9eb34f
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 18:10:00 -0600

    chatpanelnavbar icon styling

--- src/components/ChatPanelNavbar.jsx
+++ src/components/ChatPanelNavbar.jsx
@@ -1,12 +1,17 @@
 import "../styles/ChatPanelNavbar.css";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const ChatPanelNavbar = () => {
   return (
     <div className="chatpanelnavbar--container">
       <div className="chatpanelnavbar--wrapper">
+        <p className="chatpanelnavbar--user">Burak</p>
         <div className="chatpanelnavbar--icons">
-          <VideoCallIcon />
+          <VideoCallIcon fontSize="medium" />
+          <PersonAddIcon fontSize="medium" />
+          <MoreHorizIcon fontSize="medium" />
         </div>
       </div>
     </div>
