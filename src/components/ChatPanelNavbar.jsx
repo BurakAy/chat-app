a61c20a22f381e24bc744dd2adb85c2a591dcf3e
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 18:01:20 -0600

    chatpanelnavbar component and styling

--- src/components/ChatPanelNavbar.jsx
+++ src/components/ChatPanelNavbar.jsx
@@ -1,5 +1,16 @@
+import "../styles/ChatPanelNavbar.css";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+
 const ChatPanelNavbar = () => {
-  return <div></div>;
+  return (
+    <div className="chatpanelnavbar--container">
+      <div className="chatpanelnavbar--wrapper">
+        <div className="chatpanelnavbar--icons">
+          <VideoCallIcon />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPanelNavbar;
