75ea1179026915293f179382c59ded7f4caeeca0
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-11 19:10:29 -0600

    imported components

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -1,10 +1,14 @@
 import "../styles/ChatPanel.css";
 import ChatPanelNavbar from "./ChatPanelNavbar";
+import ChatPanelChat from "./ChatPanelChat";
+import MessageInput from "./MessageInput";
 
 const ChatPanel = () => {
   return (
     <div className="chatpanel--container">
       <ChatPanelNavbar />
+      <ChatPanelChat />
+      <MessageInput />
     </div>
   );
 };
