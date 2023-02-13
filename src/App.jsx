f5f1910744b9443c15a3aa9089d643fd60806888
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-12 19:21:16 -0600

    added react router dom package

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Register /> */}
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
