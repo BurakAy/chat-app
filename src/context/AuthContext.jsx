e1f148819e2cc1695f9d7d8064b2962a32b3579a
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-13 20:15:26 -0600

    removed if statement to make sure state gets set

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,24 +1,22 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
+import { app } from "../firebase";
 
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
-  const auth = getAuth();
+  const auth = getAuth(app);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setCurrentUser(user);
-      }
-      console.log(user);
+      setCurrentUser(user);
     });
 
     return () => {
       unsub();
     };
-  }, []);
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
