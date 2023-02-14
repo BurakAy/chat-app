aba41f742ea7ba4a51581f7316e1c61aa1df6a0e
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-13 22:12:28 -0600

    replacing hard coded img src and username with user object vals

--- src/components/SidebarNavbar.jsx
+++ src/components/SidebarNavbar.jsx
@@ -1,8 +1,12 @@
 import "../styles/SidebarNavbar.css";
 import userImage from "../assets/Burak_Ephesus.jpg";
 import { getAuth, signOut } from "firebase/auth";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const SidebarNavbar = () => {
+  const currentUser = useContext(AuthContext);
+
   const handleSignOut = () => {
     const auth = getAuth();
     try {
@@ -17,8 +21,11 @@ const SidebarNavbar = () => {
       <div className="side-navbar--wrapper">
         <p className="side-navbar__appname">ChatUp</p>
         <div className="side-navbar__user">
-          <img src={userImage} className="side-navbar__user-avatar" />
-          <p>Burak</p>
+          <img
+            src={currentUser.photoURL}
+            className="side-navbar__user-avatar"
+          />
+          <p>{currentUser.displayName}</p>
         </div>
         <button onClick={handleSignOut} className="side-navbar__logout">
           logout
