ce99ac16563b739cd49b15b369e985ce67380ea4
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-14 22:43:45 -0600

    user search functionality

--- src/components/SidebarSearchbar.jsx
+++ src/components/SidebarSearchbar.jsx
@@ -1,7 +1,46 @@
 import "../styles/SidebarSearchbar.css";
-import userImage from "../assets/Burak_Ephesus.jpg";
+import { useState } from "react";
+import {
+  collection,
+  getFirestore,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
 const SidebarSearchbar = () => {
+  const [userSearch, setUserSearch] = useState("");
+  const [user, setUser] = useState();
+  const [err, setErr] = useState(false);
+  const db = getFirestore();
+
+  const handleUserSearch = (event) => {
+    setUserSearch(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    if (event.key == "Enter") {
+      if (userSearch == "" && user) {
+        setUser();
+        return;
+      }
+      queryUser();
+    }
+  };
+
+  const queryUser = async () => {
+    try {
+      const usersRef = collection(db, "users");
+      const q = query(usersRef, where("displayName", "==", userSearch));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="searchbar--container">
       <div className="searchbar--wrapper">
@@ -10,12 +49,24 @@ const SidebarSearchbar = () => {
             className="finduser"
             name="finduser"
             type="text"
+            value={userSearch}
+            onKeyDown={handleSearch}
+            onChange={handleUserSearch}
             placeholder="Find a user"
           />
         </div>
         <div className="searchbar--result">
-          <img src={userImage} />
-          <p>Burak Aydemir</p>
+          {user && (
+            <>
+              <img src={user.photoURL} />
+              <p>{user.displayName}</p>
+            </>
+          )}
+          {err && (
+            <>
+              <p>User not found</p>
+            </>
+          )}
         </div>
       </div>
     </div>
