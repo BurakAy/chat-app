dc05ca170567a613766997853575ed6ed78e9eb1
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-15 20:01:03 -0600

    dynamically displaying username of chat recipient

--- src/components/ChatPanelNavbar.jsx
+++ src/components/ChatPanelNavbar.jsx
@@ -2,12 +2,15 @@ import "../styles/ChatPanelNavbar.css";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
 
 const ChatPanelNavbar = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className="chatpanelnavbar--container">
       <div className="chatpanelnavbar--wrapper">
-        <p className="chatpanelnavbar--user">Burak</p>
+        <p className="chatpanelnavbar--user">{data.user?.displayName}</p>
         <div className="chatpanelnavbar--icons">
           <VideoCallIcon fontSize="medium" />
           <PersonAddIcon fontSize="medium" />
