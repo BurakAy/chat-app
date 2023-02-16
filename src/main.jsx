61340b6073e70f5006983c4bafa294f4420f577e
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-15 19:58:53 -0600

    context provider for user chats

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <ChatContextProvider>
+        <App />
+      </ChatContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
