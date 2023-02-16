c1e4ccae057e1c10f0283ec69c2bf2d575f63ec6
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-15 20:00:00 -0600

    getting and displaying  user chats

--- src/components/ChatPanelChat.jsx
+++ src/components/ChatPanelChat.jsx
@@ -1,13 +1,30 @@
 import ChatPanelMessage from "./ChatPanelMessage";
 import "../styles/ChatPanelChat.css";
+import { useContext, useEffect, useState } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { doc, onSnapshot, getFirestore } from "firebase/firestore";
 
 const ChatPanelChat = () => {
-  return (
-    <div className="chatpanelchat--container">
-      <ChatPanelMessage />
-      <ChatPanelMessage />
-    </div>
-  );
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+  const db = getFirestore();
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [data.chatId]);
+
+  const chatMessage = messages.map((message) => {
+    console.log(message);
+    return <ChatPanelMessage message={message} key={message.id} />;
+  });
+
+  return <div className="chatpanelchat--container">{chatMessage}</div>;
 };
 
 export default ChatPanelChat;
