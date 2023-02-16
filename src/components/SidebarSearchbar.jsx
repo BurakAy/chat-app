a00c83c45a549f32952adf85e2a80092301e1fb8
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-15 20:04:32 -0600

    handling selection of chat from chats

--- src/components/SidebarSearchbar.jsx
+++ src/components/SidebarSearchbar.jsx
@@ -1,18 +1,25 @@
 import "../styles/SidebarSearchbar.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import {
   collection,
   getFirestore,
   query,
   where,
+  getDoc,
   getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
 } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
 
 const SidebarSearchbar = () => {
   const [userSearch, setUserSearch] = useState("");
   const [user, setUser] = useState();
   const [err, setErr] = useState(false);
   const db = getFirestore();
+  const currentUser = useContext(AuthContext);
 
   const handleUserSearch = (event) => {
     setUserSearch(event.target.value);
@@ -41,6 +48,41 @@ const SidebarSearchbar = () => {
     }
   };
 
+  const handleChatSelect = async () => {
+    try {
+      const combinedId =
+        currentUser.uid > user.uid
+          ? currentUser.uid + user.uid
+          : user.uid + currentUser.uid;
+      const res = await getDoc(doc(db, "chats", combinedId));
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [`${combinedId}.userInfo`]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [`${combinedId}.date`]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [`${combinedId}.userInfo`]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [`${combinedId}.date`]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setUser();
+    setUserSearch("");
+  };
+
   return (
     <div className="searchbar--container">
       <div className="searchbar--wrapper">
@@ -55,19 +97,17 @@ const SidebarSearchbar = () => {
             placeholder="Find a user"
           />
         </div>
-        <div className="searchbar--result">
-          {user && (
-            <>
-              <img src={user.photoURL} />
-              <p>{user.displayName}</p>
-            </>
-          )}
-          {err && (
-            <>
-              <p>User not found</p>
-            </>
-          )}
-        </div>
+        {user && (
+          <div className="searchbar--result" onClick={handleChatSelect}>
+            <img src={user.photoURL} />
+            <p>{user.displayName}</p>
+          </div>
+        )}
+        {err && (
+          <div className="searchbar--result">
+            <p>User not found</p>
+          </div>
+        )}
       </div>
     </div>
   );
