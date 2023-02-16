fb49ed560e7ae45c94037809ba097fa2c3592428
Burak <14030652+BurakAy@users.noreply.github.com>
2023-02-15 20:06:13 -0600

    handling selection of chat from chats

--- src/components/SidebarChats.jsx
+++ src/components/SidebarChats.jsx
@@ -1,23 +1,59 @@
 import "../styles/SidebarChats.css";
-import userImage from "../assets/Burak_Ephesus.jpg";
+import { useContext, useEffect, useState } from "react";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const SidebarChats = () => {
+  const [chats, setChats] = useState([]);
+  const db = getFirestore();
+  const currentUser = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const handleSelect = (userInfo) => {
+    dispatch({ type: "CHANGE_USER", payload: userInfo });
+  };
+
+  const Chat = () => {
+    const userChats = Object.entries(chats)?.map((chat, i) => {
+      return (
+        <div
+          className="sidebarchat"
+          key={`${chat[i]}`}
+          onClick={() => handleSelect(chat[i + 1].userInfo)}
+        >
+          <img src={`${chat[i + 1].userInfo.photoURL}`} />
+          <div className="sidebarchat--chat_info">
+            <p className="sidebarchat--user">{`${
+              chat[i + 1].userInfo.displayName
+            }`}</p>
+            <p className="sidebarchat--user_msg">
+              {`${chat[i + 1].userInfo.lastMessage?.text}`}
+            </p>
+          </div>
+        </div>
+      );
+    });
+    return userChats;
+  };
+
   return (
     <div className="sidebarchats--container">
-      <div className="sidebarchat">
-        <img src={userImage} />
-        <div className="sidebarchat--chat_info">
-          <p className="sidebarchat--user">Burak Aydemir</p>
-          <p className="sidebarchat--user_msg">Hey, what are you up to?</p>
-        </div>
-      </div>
-      <div className="sidebarchat">
-        <img src={userImage} />
-        <div className="sidebarchat--chat_info">
-          <p className="sidebarchat--user">Burak Aydemir</p>
-          <p className="sidebarchat--user_msg">Hey, what are you up to?</p>
-        </div>
-      </div>
+      <Chat />
     </div>
   );
 };
